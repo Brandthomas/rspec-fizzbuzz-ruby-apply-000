a214ec41063814342bb7b351357c2e4efa80e611
Brandon Thomas <brandonvthomas97@gmail.com>
2020-08-25 16:34:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz
+  result = []
+  for n in 1..100 do
+    result << if     n % 3 == 0 && n % 5 == 0 then "FizzBuzz"
+              elsif  n % 3 == 0               then "Fizz"
+              elsif  n % 5 == 0               then "Buzz"
+              else   n % 4 == 0               then "nil"
+              end
+  end
+  result
+end
